58cdcd7a246653b3f61ee8c2b4618ed0d15fe904
Sami Bashraheel <hello@sami.io>
2021-01-24 21:29:25 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = parseFloat(document.getElementById("principal").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let years = parseInt(document.getElementById("years").value);
+    let currentYear = new Date().getFullYear();
+    let endYear = currentYear + years;
+    let result = (principal * rate * years) / 100
+
+    document.getElementById("result").innerHTML =
+        "            If you deposit <mark>" + principal + "</mark>,<br>\n" +
+        "            at an interest rate of <mark>" + rate + "%</mark>.<br>\n" +
+        "            You will receive an amount of <mark>" + result + "</mark>,<br>\n" +
+        "            in the year <mark>" + endYear + "</mark>";
+}
+
+function rangeDisplay() {
+    document.getElementById("range-number").innerHTML = document.getElementById("rate").value + "%";
 }
         
