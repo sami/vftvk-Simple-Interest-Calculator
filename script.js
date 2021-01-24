434af0377fc88620ab5e2b205359f5d6dadfb397
Sami Bashraheel <hello@sami.io>
2021-01-24 22:40:03 +0200

    files modified to requirements

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function compute()
     let principal = parseFloat(document.getElementById("principal").value);
     let rate = parseFloat(document.getElementById("rate").value);
     let years = parseInt(document.getElementById("years").value);
+    let interest = principal * years * rate / 100;
     let currentYear = new Date().getFullYear();
-    let endYear = currentYear + years;
-    let result = (principal * rate * years) / 100
+    let futureYear = currentYear + years - 1;
 
-    if (isNaN(principal) || principal < 0) {
-        alert("Please enter a positive number");
+    if (isNaN(principal) || principal <= 0) {
+        alert("Enter a positive number");
         document.getElementById("principal").focus();
     } else {
         document.getElementById("result").innerHTML =
             "            If you deposit <mark>" + principal + "</mark>,<br>\n" +
             "            at an interest rate of <mark>" + rate + "%</mark>.<br>\n" +
-            "            You will receive an amount of <mark>" + result + "</mark>,<br>\n" +
-            "            in the year <mark>" + endYear + "</mark>";
+            "            You will receive an amount of <mark>" + interest + "</mark>,<br>\n" +
+            "            in the year <mark>" + futureYear + "</mark>";
     }
 }
 
